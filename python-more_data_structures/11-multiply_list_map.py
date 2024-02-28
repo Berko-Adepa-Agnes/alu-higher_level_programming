964fc3b72872b6997693b6c902fb364d451630a2
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-02-28 14:17:06 -0800

    Task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
+def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
+    total_score = 0
+    num_of_val = 0
+    for score, weight in my_list:
+        total_score += (score * weight)
+        num_of_val += weight
+    return total_score / num_of_val
