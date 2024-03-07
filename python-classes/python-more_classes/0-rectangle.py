541baa13f047ae0311b020fdcd1452c6359e0f83
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-03-07 04:03:26 -0800

    Task 0

--- python-classes/python-more_classes/0-rectangle.py
+++ python-classes/python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+def square_matrix_simple(matrix=[]):
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
 
-
-class Rectangle:
-    """Represent a rectangle."""
-    pass
