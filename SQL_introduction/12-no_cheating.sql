8f00c6d0944ab4b9ee5ecf7969be942f9b8da897
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-03-19 15:05:14 -0700

    Task 12

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,4 @@
--- Removes all records with a score <= 5 in the table second_table in my MySQL server.
-DELETE FROM `second_table`
-WHERE `score` <= 5;
+-- Updates the score of Bob to 10 in the table second_table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
