90439d3e8a4652e8f061551af0168b31e98af2c3
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-06-06 17:42:00 +0200

    1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,9 +2,6 @@
 const firstline = 'C is fun';
 const secondline = 'python is cool';
 const thirdline = 'JavaScript is amazing';
-if (thirdline === 'Eke has a big head') {
-    console.log(firstline)
-    console.log(secondline);
-    console.log(thirdline);
-}
-
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
