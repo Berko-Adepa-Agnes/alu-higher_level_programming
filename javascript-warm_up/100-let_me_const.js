2527629e85c35419a6cf03290837f2dc8532e693
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-06-07 20:37:32 +0200

    100-let_me_const.js

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const myVar = 333; // Define the variable
+const myVar = 40; // Define the variable
 Object.freeze(myVar); // Attempting to freeze a primitive value (which is not necessary)
 console.log(myVar); // Use the variable
