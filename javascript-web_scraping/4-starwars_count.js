cd50b05f742a91b7c16a91f1c7c46840ee1f66c7
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-06-08 12:51:22 +0200

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const fs = require('fs');
+let nFilms = 0;
 
 request(process.argv[2], function (err, response, body) {
   if (err == null) {
-    fs.writeFileSync(process.argv[3], body);
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          nFilms++;
+        }
+      }
+    }
   }
+  console.log(nFilms);
 });
