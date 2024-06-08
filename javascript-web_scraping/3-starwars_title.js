ff25add9015e70b82d74e802a325b25510409769
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-06-08 12:28:21 +0200

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,18 @@ const id = process.argv[2];
 const url = 'https://swapi.dev/api/films/' + id + '/';
 
 request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (error) {
+    console.error('Request error:', error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode);
+    return;
+  }
+  try {
     const json = JSON.parse(body);
     console.log(json.title);
-  } else {
-    console.error('Error:', error);
+  } catch (parseError) {
+    console.error('JSON parse error:', parseError);
   }
 });
