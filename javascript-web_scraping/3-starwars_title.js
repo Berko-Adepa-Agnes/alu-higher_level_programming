e6c22eefec2f337694142e600b5a4a221ac91878
Berko-Adepa-Agnes <156245300+Berko-Adepa-Agnes@users.noreply.github.com>
2024-06-09 01:19:38 +0400

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,21 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi.dev/api/films/' + id + '/';
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('Request error:', error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error(`Invalid status code: ${response.statusCode}`);
-    return;
-  }
-  try {
-    const json = JSON.parse(body);
-    console.log(json.title);
-  } catch (parseError) {
-    console.error('JSON parse error:', parseError);
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
   }
 });
