05ba3a14518ae6a88e68ef60936e2055304ba7dc
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-06-08 12:39:46 +0200

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-const url = `https://swapi.dev/api/films/${id}/`;
+const url = 'https://swapi.dev/api/films/' + id + '/';
 
 request(url, function (error, response, body) {
   if (error) {
     console.error('Request error:', error);
     return;
   }
-  console.log('Response status code:', response.statusCode);
   if (response.statusCode !== 200) {
     console.error(`Invalid status code: ${response.statusCode}`);
-    console.error('Response body:', body); // Log the body for further inspection
     return;
   }
   try {
